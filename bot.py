9ba747acc4b3cb4b6aa3487cc109898851518aae
HacketOS <mr.tuniguk@gmail.com>
2019-08-12 21:35:24 +0300

    fix bug with threads

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import telebot
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
+import tensorflow as tf
 
 def load_image(path, size = [150,150]):
   img = load_img(path, target_size = size)
   return img_to_array(img).reshape(1,150,150,3)
 
-catdog_classifier = load_model('CatDog.h5')
+catdog_classifier = load_model('CatDog.h5', compile= False)
+graph = tf.get_default_graph()
 
 _token = '831937104:AAEdZQq6_CrpnkIVmteJPHYzBl6nkRl2AwM'
 
@@ -19,13 +21,15 @@ def start_message(message):
 @bot.message_handler(content_types=['photo'])
 def send_message(message):
     raw = message.photo[-1].file_id
-    name = raw+".jpg"
+    name = raw + ".jpg"
     file_info = bot.get_file(raw)
     downloaded_file = bot.download_file(file_info.file_path)
     with open(name,'wb') as new_file:
         new_file.write(downloaded_file)
     img = load_image(name)
-    answer = catdog_classifier.predict(img)[0]
+    global graph
+    with graph.as_default():
+      answer = catdog_classifier.predict(img)[0]
     f_answer = 'cat: %.3f \ndog: %.3f' % tuple(answer)
     bot.send_message(message.chat.id, f_answer)
 bot.polling()
