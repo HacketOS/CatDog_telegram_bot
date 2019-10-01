2b42e5bf58f20568cde28b09a6192951785d0cf9
HacketOS <mr.tuniguk@gmail.com>
2019-10-01 23:20:22 +0300

    increase accuracy of model and fix code style

--- bot.py
+++ bot.py
@@ -4,9 +4,10 @@ from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import tensorflow as tf
 
-def load_image(path, size = [250,250]):
-  img = load_img(path, target_size = size)
-  return img_to_array(img).reshape(1,size[0],size[1],3)
+
+def load_image(path, size=[250, 250]):
+    img = load_img(path, target_size=size)
+    return img_to_array(img).reshape(1, size[0], size[1], 3)
 
 catdog_classifier = load_model('CatDog.h5', compile= False)
 graph = tf.get_default_graph()
@@ -14,9 +15,12 @@ graph = tf.get_default_graph()
 _token = '831937104:AAEdZQq6_CrpnkIVmteJPHYzBl6nkRl2AwM'
 
 bot = telebot.TeleBot(_token)
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id, 'Чё каво чепушило')
+    bot.send_message(message.chat.id, 'Чё каво чепушило')
+
 
 @bot.message_handler(content_types=['photo'])
 def send_message(message):
@@ -24,12 +28,12 @@ def send_message(message):
     name = raw + ".jpg"
     file_info = bot.get_file(raw)
     downloaded_file = bot.download_file(file_info.file_path)
-    with open(name,'wb') as new_file:
+    with open(name, 'wb') as new_file:
         new_file.write(downloaded_file)
     img = load_image(name)
     global graph
     with graph.as_default():
-      answer = catdog_classifier.predict(img)[0]
+        answer = catdog_classifier.predict(img)[0]
     f_answer = 'cat: %.3f \ndog: %.3f' % tuple(answer)
     bot.send_message(message.chat.id, f_answer)
 bot.polling()
